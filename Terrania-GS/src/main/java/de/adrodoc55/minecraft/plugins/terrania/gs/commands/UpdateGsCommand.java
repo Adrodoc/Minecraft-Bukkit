01b6783d676bc86acb522948347d15e154096cfd
Adrodoc55 <adrodoc55@googlemail.com>
2016-01-04 01:59:04 +0100

    now using updateAlleGrundstücke method in update command

--- Terrania-GS/src/main/java/de/adrodoc55/minecraft/plugins/terrania/gs/commands/UpdateGsCommand.java
+++ Terrania-GS/src/main/java/de/adrodoc55/minecraft/plugins/terrania/gs/commands/UpdateGsCommand.java
@@ -2,25 +2,22 @@ package de.adrodoc55.minecraft.plugins.terrania.gs.commands;
 
 import de.adrodoc55.minecraft.plugins.common.command.CommandContext;
 import de.adrodoc55.minecraft.plugins.common.utils.MinecraftUtils;
-import de.adrodoc55.minecraft.plugins.terrania.gs.Grundstueck;
 import de.adrodoc55.minecraft.plugins.terrania.gs.GsManager;
 
 public class UpdateGsCommand extends ParamlessGsCommand {
 
-	protected UpdateGsCommand() {
-		super("update");
-	}
+    protected UpdateGsCommand() {
+        super("update");
+    }
 
-	@Override
-	protected boolean execute(CommandContext context) {
-		for (GsManager gsm : GsManager.getActiveInstances()) {
-			for (Grundstueck gs : gsm.getGrundstuecke()) {
-				gs.updateSignConent();
-			}
-		}
-		String message = "Alle Grundstücke wurden geupdated.";
-		MinecraftUtils.sendInfo(context.getSender(), message);
-		return true;
-	}
+    @Override
+    protected boolean execute(CommandContext context) {
+        for (GsManager gsm : GsManager.getActiveInstances()) {
+            gsm.updateAlleGrundstuecke();
+        }
+        String message = "Alle Grundstücke wurden geupdated.";
+        MinecraftUtils.sendInfo(context.getSender(), message);
+        return true;
+    }
 
 }
