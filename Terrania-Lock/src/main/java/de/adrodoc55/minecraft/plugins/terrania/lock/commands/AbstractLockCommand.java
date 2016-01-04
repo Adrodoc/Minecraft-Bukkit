16b98573ebac3b243bb56918b3b467f7db89924f
Adrodoc55 <adrodoc55@googlemail.com>
2016-01-04 01:59:54 +0100

    LockCommand fixed for server bukkit version

--- Terrania-Lock/src/main/java/de/adrodoc55/minecraft/plugins/terrania/lock/commands/AbstractLockCommand.java
+++ Terrania-Lock/src/main/java/de/adrodoc55/minecraft/plugins/terrania/lock/commands/AbstractLockCommand.java
@@ -1,12 +1,9 @@
 package de.adrodoc55.minecraft.plugins.terrania.lock.commands;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -21,66 +18,67 @@ import de.adrodoc55.minecraft.plugins.common.utils.MinecraftUtils;
 
 public abstract class AbstractLockCommand implements CommandHandler {
 
-	private final String name;
+    private final String name;
 
-	public AbstractLockCommand(String name) {
-		this.name = name;
-	}
+    public AbstractLockCommand(String name) {
+        this.name = name;
+    }
 
-	private String getName() {
-		return name;
-	}
+    private String getName() {
+        return name;
+    }
 
-	private final String getUsage() {
-		return ChatColor.RED + "/" + getName();
-	}
+    private final String getUsage() {
+        return ChatColor.RED + "/" + getName();
+    }
 
-	@Override
-	public final boolean onCommand(CommandSender sender, Command command,
-			String label, String[] args) {
-		command.setUsage(getUsage());
+    @Override
+    public final boolean onCommand(CommandSender sender, Command command,
+            String label, String[] args) {
+        command.setUsage(getUsage());
 
-		ParameterList paramDef = new ParameterList();
-		CommandContext context;
-		try {
-			context = new CommandContext(sender, command, label, args, paramDef);
-		} catch (ParameterException ex) {
-			MinecraftUtils.sendError(sender, ex.getMessage());
-			return false;
-		}
+        ParameterList paramDef = new ParameterList();
+        CommandContext context;
+        try {
+            context = new CommandContext(sender, command, label, args, paramDef);
+        } catch (ParameterException ex) {
+            MinecraftUtils.sendError(sender, ex.getMessage());
+            return false;
+        }
 
-		command.setUsage("");
-		Player player;
-		try {
-			player = (Player) sender;
-		} catch (ClassCastException ex) {
-			String message = "Dieser Befehl kann nur von Spielern ausgeführt werden.";
-			MinecraftUtils.sendError(sender, message);
-			return false;
-		}
-		Set<Material> transparent = new HashSet<Material>();
-		transparent.add(Material.AIR);
-		Block targetBlock = player.getTargetBlock(transparent, 10);
-		if (targetBlock == null) {
-			String message = "Du musst einen Block ansehen um diesen Befehl auszuführen.";
-			MinecraftUtils.sendError(player, message);
-			return false;
-		}
-		if (!MaterialUtils.isLockable(targetBlock.getType())) {
-			String message = "Solch ein Block kann nicht abgeschlossen werden.";
-			MinecraftUtils.sendError(player, message);
-			return false;
-		}
-		return execute(context, player, targetBlock);
-	}
+        command.setUsage("");
+        Player player;
+        try {
+            player = (Player) sender;
+        } catch (ClassCastException ex) {
+            String message = "Dieser Befehl kann nur von Spielern ausgeführt werden.";
+            MinecraftUtils.sendError(sender, message);
+            return false;
+        }
+        // Set<Material> transparent = new HashSet<Material>();
+        // transparent.add(Material.AIR);
+        @SuppressWarnings("deprecation")
+        Block targetBlock = player.getTargetBlock(null, 10);
+        if (targetBlock == null) {
+            String message = "Du musst einen Block ansehen um diesen Befehl auszuführen.";
+            MinecraftUtils.sendError(player, message);
+            return false;
+        }
+        if (!MaterialUtils.isLockable(targetBlock.getType())) {
+            String message = "Solch ein Block kann nicht abgeschlossen werden.";
+            MinecraftUtils.sendError(player, message);
+            return false;
+        }
+        return execute(context, player, targetBlock);
+    }
 
-	public abstract boolean execute(CommandContext context, Player player,
-			Block block);
+    public abstract boolean execute(CommandContext context, Player player,
+            Block block);
 
-	@Override
-	public final List<String> onTabComplete(CommandSender sender,
-			Command command, String alias, String[] args) {
-		return new ArrayList<String>(0);
-	}
+    @Override
+    public final List<String> onTabComplete(CommandSender sender,
+            Command command, String alias, String[] args) {
+        return new ArrayList<String>(0);
+    }
 
 }
